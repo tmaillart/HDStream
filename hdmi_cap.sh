beee73692f37f76e07f206f1ed8929f2a04613b8
Théo MAILLART <tmaillart@gmail.com>
2022-01-15 15:46:32 +0100

    custom for rpi

--- hdmi_cap.sh
+++ hdmi_cap.sh
@@ -1,30 +1,40 @@
 #!/bin/sh
 
-export GST_DEBUG=*:3
+export GST_DEBUG=*:3,v4l2src:1
+
+trap 'killall busybox' EXIT
 
 rm *.ts
 # add -f for foreground
 busybox httpd -p 0.0.0.0:8080
 gst-launch-1.0 \
 	v4l2src \
-		device=/dev/video2 \
+		device=/dev/video0 \
 	! image/jpeg,width=1280,height=720,framerate=30/1 \
 	! jpegdec \
+	! videoscale \
+		method=0 \
+	! video/x-raw,width=720,height=480 \
 	! x264enc \
 		tune=zerolatency \
+		speed-preset=3 \
 	! queue \
 	! mpegtsmux \
 		name=mux \
 	! hlssink \
-		max-files=5 \
+		max-files=3 \
 	alsasrc \
-		device=hw:1,0 \
+		device=hw:2,0 \
 	! audio/x-raw,format=S16LE,channels=2,rate=48000 \
+	! audioconvert \
+	! audioresample \
+		resample-method=1 \
+	! audio/x-raw,format=S16LE,channels=1,rate=44100 \
 	! lamemp3enc bitrate=128 \
 	! mpegaudioparse \
 	! queue \
 	! mux.
-killall busybox
+
 exit
 	! xvimagesink \
 	\
