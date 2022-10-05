1147025ff05451b929cee13141c11c2673d32950
Théo Maillart <tmaillart@freebox.fr>
2022-10-05 08:53:42 +0200

    60 fps is supported and way better

--- capture.sh
+++ capture.sh
@@ -6,7 +6,7 @@ AUDIO_DEV=alsa_input.usb-MACROSILICON_USB_Video-02.iec958-stereo
 GST_DEBUG=*:3 gst-launch-1.0 -e -v \
 	v4l2src \
 		"device=$VIDEO_DEV" \
-	! image/jpeg,width=1280,height=720,framerate=20/1 \
+	! image/jpeg,width=1280,height=720,framerate=60/1 \
 	! jpegdec \
 	! videoconvert \
 	! tee \
