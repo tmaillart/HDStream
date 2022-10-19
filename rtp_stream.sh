ee86b7bba9a548388ba0e7a82554baea3fb88b5f
Théo Maillart <tmaillart@freebox.fr>
2022-10-19 15:21:40 +0200

    rtp fix timestamp consistency

--- rtp_stream.sh
+++ rtp_stream.sh
@@ -3,11 +3,11 @@
 export GST_DEBUG=*:3,v4l2src:1
 
 #V4L2_DEV='/dev/video0'
-#PULSE_DEV='alsa_input.usb-MACROSILICON_USB3._0_capture-02.iec958-stereo'
-if [ -z "$PULSE_DEV" ] || ! pactl get-source-volume "$PULSE_DEV" >/dev/null 2>/dev/null;then
-	pactl list short sources
+#ALSA_DEV='alsa_input.usb-MACROSILICON_USB3._0_capture-02.iec958-stereo'
+if [ -z "$ALSA_DEV" ] || ! [ -e "/dev/snd/$(echo "$ALSA_DEV" | sed 's/hw:/pcmC/ ; s/,/D/')c" ] ;then
+	cat /proc/asound/cards
 	printf '\nselect: '
-	read PULSE_DEV
+	read ALSA_DEV
 fi
 
 if [ -z "$V4L2_DEV" ];then
@@ -17,14 +17,15 @@ fi
 
 gst-rtsp-launch -a 0.0.0.0 "( \
 	v4l2src \
-		io-mode=2 \
 		device=$V4L2_DEV \
 	! image/jpeg,width=1280,height=720,framerate=60/1 \
 	! rtpjpegpay \
 		name=pay0 \
-        pulsesrc \
-		device=$PULSE_DEV \
-	! audio/x-raw,format=S16LE,rate=44100 \
+        alsasrc \
+		device=$ALSA_DEV \
+		provide-clock=false \
+		use-driver-timestamps=true \
+	! audio/x-raw,format=S16LE,rate=48000,channels=2 \
 	! lamemp3enc \
 	! mpegaudioparse \
 	! rtpmpapay \
